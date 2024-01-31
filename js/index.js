657df5c846eee656aa1896c144a2d36516880cc5
Lalehtavakoli <lalehtavakoli58@gmail.com>
2024-01-30 21:10:23 -0500

    updated

---
+++ js/index.js
@@ -0,0 +1,7 @@
+console.log("Your index.js file is loaded correctly!");
+$(document).ready(function() {
+    $(".learnMoreButton").on("click", function() {
+      // Your code here to handle the click event
+      alert("Learn More button clicked!");
+    });
+  });
