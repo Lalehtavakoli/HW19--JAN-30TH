f33639a4cc88f6a597a46baaded561a6503984f9
Lalehtavakoli <lalehtavakoli58@gmail.com>
2024-01-30 21:15:38 -0500

    fixed

--- js/index.js
+++ js/index.js
@@ -1,7 +1,25 @@
 console.log("Your index.js file is loaded correctly!");
+
+
 $(document).ready(function() {
-    $(".learnMoreButton").on("click", function() {
-      // Your code here to handle the click event
-      alert("Learn More button clicked!");
+    var learnMoreBtn = $("#learnMoreBtn");
+  
+    // When the user scrolls down 20px from the top of the document, show the button
+    $(window).scroll(function() {
+      scrollFunction();
+    });
+  
+    function scrollFunction() {
+      if ($(document).scrollTop() > 20) {
+        mybutton.css("display", "block");
+      } else {
+        mybutton.css("display", "none");
+      }
+    }
+  
+    // When the user clicks on the button, scroll to the top of the document
+    mybutton.click(function() {
+      $("body,html").animate({ scrollTop: 0 }, "slow");
     });
   });
+  
